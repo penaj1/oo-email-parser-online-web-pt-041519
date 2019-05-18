5f07f76033173f3b3a1de14a31a5452f17363590
Janice Pena <jipena94@gmail.com>
2019-05-18 18:33:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailParser
   end
   
   def parse
-    puts splitEmail = email.split.collect {|address| address.split
+    puts splitEmail = email.split.collect {|address| address.split(', ')
   end
 
 end
